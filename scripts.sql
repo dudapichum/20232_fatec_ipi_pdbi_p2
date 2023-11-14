0f7cd94ddaf89f030383e20af85b906259e0a84a
Maria Eduarda Ribeiro <maria.ribeiro33@fatec.sp.gov.br>
2023-11-14 10:03:18 -0300

    feat(tg): valores negativos jamais

--- scripts.sql
+++ scripts.sql
@@ -1,24 +1,46 @@
+-- 2. Trigger
+CREATE OR REPLACE TRIGGER tg_antes_do_insert
+BEFORE INSERT OR UPDATE ON tb_youtubers
+FOR EACH ROW 
+EXECUTE PROCEDURE fn_valor_negativo();
+		
+
+CREATE OR REPLACE FUNCTION fn_valor_negativo()
+RETURNS TRIGGER LANGUAGE plpgsql 
+AS $$
+BEGIN
+    IF (NEW.subscribe < 0 OR NEW.video_views < 0 OR NEW.video_count < 0) THEN
+        RAISE EXCEPTION 'Valores negativos não são permitidos';
+    END IF;
+    RETURN NEW;
+END;
+$$						  
+	
+INSERT INTO tb_youtubers(rank, youtuber, subscribe, video_views, video_count, category, started) 
+VALUES(101, 'BOT', 10000000, 23908756, 50, 'Shows', '2016');
+
+
 -- 1.Base de dados e criação de tabela
-CREATE TABLE tb_youtubers(
-	cod_youtubers SERIAL PRIMARY KEY,
-	rank INT NOT NULL,
-	youtuber VARCHAR(200) NOT NULL,
-	subscribe INT NOT NULL,
-	video_views INT NOT NULL,
-	video_count INT NOT NULL,
-	category VARCHAR(200) NOT NULL,
-	started VARCHAR(4) NOT NULL
-);
+-- CREATE TABLE tb_youtubers(
+-- 	cod_youtubers SERIAL PRIMARY KEY,
+-- 	rank INT NOT NULL,
+-- 	youtuber VARCHAR(200) NOT NULL,
+-- 	subscribe INT NOT NULL,
+-- 	video_views INT NOT NULL,
+-- 	video_count INT NOT NULL,
+-- 	category VARCHAR(200) NOT NULL,
+-- 	started VARCHAR(4) NOT NULL
+-- );
 
-SELECT * FROM tb_youtubers;
+-- SELECT * FROM tb_youtubers;
 
-ALTER TABLE tb_youtubers
-ALTER COLUMN started TYPE VARCHAR(200);
+-- ALTER TABLE tb_youtubers
+-- ALTER COLUMN started TYPE VARCHAR(200);
 
-ALTER TABLE tb_youtubers
-ALTER COLUMN subscribe TYPE BIGINT;
+-- ALTER TABLE tb_youtubers
+-- ALTER COLUMN subscribe TYPE BIGINT;
 
-ALTER TABLE tb_youtubers
-ALTER COLUMN video_views TYPE BIGINT;
+-- ALTER TABLE tb_youtubers
+-- ALTER COLUMN video_views TYPE BIGINT;
 
 -- Atividade Avaliativa - P2
